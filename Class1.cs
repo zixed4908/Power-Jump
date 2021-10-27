29d518d26c617ef012c5b417674089b4c65d0fd9
zixed4908 <76135752+zixed4908@users.noreply.github.com>
2021-10-26 20:41:14 -0400

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -11,39 +11,44 @@ using System.Net;
 using Photon.Realtime;
 using UnityEngine.Rendering;
 
-namespace MonkeHaveLoooooooongArms_
-{
-        [BepInPlugin("org.ZixedTheMonkeGuy.monkeytag.MonkeHaveLoooooooongArms", "Monke Have Looong Arms!", "Version 1.0.0")]
-        public class MyPatcher : BaseUnityPlugin
-        {
-            public void Awake()
-            {
-                var harmony = new Harmony("com.ZixedTheMonkeGuy.monkeytag");
-                harmony.PatchAll(Assembly.GetExecutingAssembly());
-            }
-        }
-        public class Class1
-        {
-             static bool longarms = false;
-             static void Postfix(GorillaLocomotion.Player __instance)
-             {
-                  if(!PhotonNetwork.CurrentRoom.IsVisible || !PhotonNetwork.InRoom)
-                  {
-                        List<InputDevice> list = new List<InputDevice>();
-                        InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
-                        list[0].TryGetFeatureValue(CommonUsages.gripButton, out longarms);
 
-                         if(longarms)
-                         {
-                             __instance.maxArmLength = 500f;
-                             __instance.bodyCollider.attachedRigidbody.useGravity = false;
-                         }
-                         else
-                         {
-                             __instance.maxArmLength = 1.1f;
-                             __instance.bodyCollider.attachedRigidbody.useGravity = true;
-                         }
-                  }
-             }
+
+namespace PowerJump
+{
+    [BepInPlugin("org.ZixedTheMonkeGuy.monkeytag.PowerJump", "Power Jump", "0.1.0.0")]
+    public class MyPatcher : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            var harmony = new Harmony("com.ZixedTheMonkeGuy.monkeytag.PowerJump");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
+    }
+
+    [HarmonyPatch(typeof(GorillaLocomotion.Player))]
+    [HarmonyPatch("Update", MethodType.Normal)]
+    public class Class1
+    {
+        static bool jumpBoost = false;
+
+        static void Postfix(GorillaLocomotion.Player __instance)
+        {
+            if (!PhotonNetwork.CurrentRoom.IsVisible || !PhotonNetwork.InRoom)
+            {
+                List<InputDevice> list = new List<InputDevice>();
+                InputDevices.GetDevicesWithCharacteristics(UnityEngine.XR.InputDeviceCharacteristics.HeldInHand | UnityEngine.XR.InputDeviceCharacteristics.Right | UnityEngine.XR.InputDeviceCharacteristics.Controller, list);
+                list[0].TryGetFeatureValue(CommonUsages.gripButton, out jumpBoost);
+
+                if (jumpBoost)
+                {
+                    __instance.maxJumpSpeed = 999f;
+                }
+                else
+                {
+                    __instance.maxJumpSpeed = 1.1f;
+                }
+            }
+
+        }
+    }
 }
